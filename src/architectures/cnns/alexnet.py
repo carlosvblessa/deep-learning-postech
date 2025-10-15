9d6cff1926d8b1bad837ef216c9f354455cede52
Carlos Virgílio Beltrão Lessa <carloslessa@hotmail.com>
2025-10-15 15:02:29 -0300

    Desativada a detecção de gpu.

--- src/architectures/cnns/alexnet.py
+++ src/architectures/cnns/alexnet.py
@@ -7,7 +7,7 @@ import mlflow
 import mlflow.pytorch
 
 # Set device configuration
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu")
 torch.set_num_threads(os.cpu_count())
 
 # Hyperparameters
